245fe6559fe190604caa0fc78712f0a509970a2e
melodie <melodie.bernard123@yahoo.fr>
2024-08-16 23:15:46 +0200

    ex00 to check, seems done

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,21 +6,44 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 10:16:27 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/15 12:48:32 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/08/16 23:14:49 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "../includes/Fixed.hpp"
+#include "../includes/ClapTrap.hpp"
 
 int main(void) {
-    Fixed a;
-    Fixed b(a);
-    Fixed c;
+    ClapTrap jim("Jim");
+    ClapTrap bob("Bob");
+    ClapTrap jim2(jim);
+    ClapTrap alex("Alex");
+    ClapTrap clover("Clover");
 
-    c = b;
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
+    for (int i = 0; i < 10; i ++) {
+        jim.attack("Bob");
+        bob.takeDamage(jim.getAttackDamage());
+    }
+    jim.attack("Bob");
+    jim2.attack("Jim");
+    jim.takeDamage(jim2.getAttackDamage());
+    bob.beRepaired(0);
+    jim.beRepaired(1);
+    jim.attack("Bob");
+    jim.attack("Jim");
+    jim.takeDamage(10);
+    jim.beRepaired(10);
+    jim.takeDamage(1);
+    jim.attack("Bob");
+    for (int i = 0; i < 10; i ++) {
+        alex.attack("Bob");
+        bob.takeDamage(1);
+        bob.beRepaired(1);
+    }
+    for (int i = 0; i < 10; i ++) {
+        clover.attack("Bob");
+        bob.takeDamage(1);
+        bob.beRepaired(1);
+    }
     return (0);
 }
