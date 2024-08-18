71a579ebb8d11b21b74c247c3541e1c507d8fee1
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-08-18 16:41:04 +0200

    ex01 to check

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 10:16:27 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/18 15:23:53 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/08/18 16:39:42 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 static void   scavLoopAttack(ScavTrap *att, ScavTrap *def, std::string name) {
     for (int i = 0; i < 10; i ++) {
         att->attack(name);
-        def->takeDamage(att->getAttackDamage());
+        if (att->getHitPoints() > 0)
+            def->takeDamage(att->getAttackDamage());
         def->beRepaired(1);
     }
 }
@@ -25,12 +26,13 @@ static void   scavLoopAttack(ScavTrap *att, ScavTrap *def, std::string name) {
 int    main(void) {
     ClapTrap jim("Jim");
     ClapTrap bob("Bob");
+    ScavTrap clover("Clover");
     ScavTrap alex("Alex");
     ScavTrap alex2(alex);
 
-    alex2.guardGate();
-    scavLoopAttack(&alex, &alex2, "alex2");
-    scavLoopAttack(&alex2, &alex, "alex");
+    clover.guardGate();
+    scavLoopAttack(&alex, &alex2, "its copy : Alex2");
+    scavLoopAttack(&alex2, &alex, "the original Alex");
     alex2.guardGate();
     jim.attack("Bob");
     return (0);
